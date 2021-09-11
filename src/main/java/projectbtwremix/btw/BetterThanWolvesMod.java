2616b6403f2cdeceee5798ba52ab4480719b858d
Cocona <pexium128@gmail.com>
2021-09-11 19:53:31 -0400

    Cleanup template project to use correct namespace, version, etc

--- src/main/java/projectbtwremix/btw/BetterThanWolvesMod.java
+++ src/main/java/projectbtwremix/btw/BetterThanWolvesMod.java
@@ -1,14 +1,11 @@
-package net.fabricmc.example;
+package projectbtwremix.btw;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class BetterThanWolvesMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
 	}
 }
